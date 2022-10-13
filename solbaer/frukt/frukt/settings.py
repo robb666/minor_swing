7e1229f531882f20cac9146d964d07241ddd04ea
Robb <56496611+robb666@users.noreply.github.com>
2022-10-13 22:04:11 +0200

    Update settings.py

--- solbaer/frukt/frukt/settings.py
+++ solbaer/frukt/frukt/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from constants import pass_
+from constants import secret, pass_
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9%24l-t@=ao=y^doxxzxzm68icv60b+j&31#ffnp0!w4#pea5='
+SECRET_KEY = secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
