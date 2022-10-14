e594cd9be199099c2e969ca20ddbeb1094ae6a81
Robb <56496611+robb666@users.noreply.github.com>
2022-10-14 16:38:30 +0200

    up

--- solbaer/frukt/blog/models.py
+++ solbaer/frukt/blog/models.py
@@ -9,10 +9,14 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True, upload_to='profile_pics')
 
     def __str__(self):
         return self.title
+
+    # def __str__(self):
+    #     return f'{self.user}'
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
--- solbaer/frukt/frukt/urls.py
+++ solbaer/frukt/frukt/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from contact.views import index
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('contact/', index, name='index')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# if settings.DEBUG:
+#     static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
