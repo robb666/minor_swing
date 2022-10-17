845b4fd7f9d26bccccfb9a89c8a00e43019d89e1
Robb <56496611+robb666@users.noreply.github.com>
2022-10-17 20:36:15 +0200

    up

--- solbaer/frukt/contact/forms.py
+++ solbaer/frukt/contact/forms.py
@@ -3,5 +3,13 @@ from django import forms
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    inquiry = forms.CharField(widget=forms.Textarea)
+    email = forms.EmailField(
+        widget=forms.TextInput(attrs={"id": "email", "onkeyup": "validateEmail()"}),
+    )
+
+    inquiry = forms.CharField(widget=forms.Textarea(attrs={'rows': 10}))
+
+
+
+
+
