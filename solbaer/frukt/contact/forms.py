1c587498dad20a10ede08eca8aaabcc8307c9da6
Robb <56496611+robb666@users.noreply.github.com>
2022-10-17 21:08:49 +0200

    up

--- solbaer/frukt/contact/forms.py
+++ solbaer/frukt/contact/forms.py
@@ -1,12 +1,12 @@
 from django import forms
-
+from django.db import models
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
     email = forms.EmailField(
         widget=forms.TextInput(attrs={"id": "email", "onkeyup": "validateEmail()"}),
     )
-
+    # record = forms.BooleanField()
     inquiry = forms.CharField(widget=forms.Textarea(attrs={'rows': 10}))
 
 
--- solbaer/frukt/contact/views.py
+++ solbaer/frukt/contact/views.py
@@ -7,6 +7,7 @@ from django.contrib import messages
 
 # Create your views here.
 def index(request):
+
     if request.method == 'POST':
         form = ContactForm(request.POST)
 
