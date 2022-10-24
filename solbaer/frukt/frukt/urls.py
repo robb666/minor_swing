6ac31f03cde3a49ce623afdea0339ac4c03aee1c
Robb <56496611+robb666@users.noreply.github.com>
2022-10-24 21:34:03 +0200

    up

--- solbaer/frukt/frukt/urls.py
+++ solbaer/frukt/frukt/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('contact/', index, name='index')
+    path('contact/', index, name='index'),
 ]
 
 if settings.DEBUG:
