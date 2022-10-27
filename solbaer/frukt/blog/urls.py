d4e47f7e8ec2e85e870d2534ba3bc6c340173285
Robb <56496611+robb666@users.noreply.github.com>
2022-10-27 09:52:07 +0200

    up

--- solbaer/frukt/blog/urls.py
+++ solbaer/frukt/blog/urls.py
@@ -9,13 +9,13 @@ from contact.views import index
 
 
 
+
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about'),
     path('contact/', index, name='form-contact'),
 ]
-
 
 
 # if settings.DEBUG:
