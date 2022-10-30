81457a811b7f4a0c5b974effc249a9dae2389f52
Robb <56496611+robb666@users.noreply.github.com>
2022-10-30 11:17:00 +0100

    Update views.py

--- solbaer/frukt/blog/views.py
+++ solbaer/frukt/blog/views.py
@@ -20,14 +20,15 @@ class SearchBar(ListView):
     template_name = 'blog/search.html'
     context_object_name = 'posts'
     queryset = Post.objects.filter(status=1)
-    ordering = ['-date_posted']
-    paginate_by = 1
+
+    paginate_by = 4
 
     def get_queryset(self):
         query = self.request.GET.get('q')
         object_list = Post.objects.filter(
             Q(title__icontains=query) |
-            Q(content__icontains=query))
+            Q(content__icontains=query)).order_by('-date_posted')
+
         return object_list
 
 
