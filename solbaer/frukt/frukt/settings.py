20665076a03498a06b2935883176381bb10d812b
robb666 <56496611+robb666@users.noreply.github.com>
2022-11-17 20:20:44 +0100

    up

--- solbaer/frukt/frukt/settings.py
+++ solbaer/frukt/frukt/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
     'contact',
     'storages',
     'ckeditor',
