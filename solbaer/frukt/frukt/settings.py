97aea489098aa967c94df25041fdfca9bc259b58
robb666 <56496611+robb666@users.noreply.github.com>
2022-11-27 14:18:45 +0100

    Update settings.py

--- solbaer/frukt/frukt/settings.py
+++ solbaer/frukt/frukt/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = os.environ.get("SETTINGS_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
