e5ed3389ab421246193a26d5acc36a58ce09f759
Robb <56496611+robb666@users.noreply.github.com>
2022-11-29 22:08:55 +0100

    up

--- solbaer/frukt/frukt/settings.py
+++ solbaer/frukt/frukt/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SETTINGS_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
