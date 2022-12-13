7c694aaa40093bef01f3da8ed7a349830be4b15e
Robb <56496611+robb666@users.noreply.github.com>
2022-12-13 14:20:36 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email #$SUPER_USER_EMAIL --noinput
+#DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email #$SUPER_USER_EMAIL --noinput
 
 gunicorn frukt.wsgi:application --bind 0.0.0.0:8080
