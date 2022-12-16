e86ce8c80482b4b8d4cf90005b4aa18f06496b64
Robb <56496611+robb666@users.noreply.github.com>
2022-12-17 00:30:36 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ python manage.py collectstatic --no-input
 
 #DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email #$SUPER_USER_EMAIL --noinput
 
-gunicorn frukt.wsgi:application --bind ec2-16-171-20-189.eu-north-1.compute.amazonaws.com:8080
+gunicorn frukt.wsgi:application --bind 0.0.0.0:8080 --timeout 0
