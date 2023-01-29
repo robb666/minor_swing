3d3e4717269454138f9fc422080f86644a7d484f
Robb <56496611+robb666@users.noreply.github.com>
2023-01-29 22:49:55 +0100

    up

--- frukt/frukt/settings.py
+++ frukt/frukt/settings.py
@@ -130,11 +130,11 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
 EMAIL_HOST = 'smtp.mailgun.org'
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_PORT = 443
+EMAIL_PORT = 587
 
 
 USE_S3 = True
@@ -196,13 +196,13 @@ RUN_SERVER_PORT = 8080
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
-CSRF_TRUSTED_ORIGINS = ['https://blackcurrant-wholesale.com']
+CSRF_TRUSTED_ORIGINS = ['https://d278n61fpa957h.cloudfront.net']
 
 
 ## deployment
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
+SECURE_SSL_REDIRECT = False
 
 
 
