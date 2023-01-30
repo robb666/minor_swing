795fabf1deb15282b7299e1c19e918b19111b741
Robb <56496611+robb666@users.noreply.github.com>
2023-01-30 13:43:18 +0100

    Update settings.py

--- frukt/frukt/settings.py
+++ frukt/frukt/settings.py
@@ -130,11 +130,11 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_USE_TLS = False
+EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.mailgun.org'
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_PORT = 587
+EMAIL_PORT = 443
 
 
 USE_S3 = True
