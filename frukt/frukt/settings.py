e33f78f89bcc29b42ec77deb0b6f8be9a75cf23b
Robb <56496611+robb666@users.noreply.github.com>
2023-01-31 23:55:59 +0100

    up

--- frukt/frukt/settings.py
+++ frukt/frukt/settings.py
@@ -144,6 +144,7 @@ if USE_S3:
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+    AWS_CLOUDFRONT_DOMAIN = 'd278n61fpa957h.cloudfront.net'
     AWS_DEFAULT_ACL = 'public-read'
 
     AWS_S3_FILE_OVERWRITE = True
--- frukt/frukt/storages.py
+++ frukt/frukt/storages.py
@@ -6,10 +6,17 @@ from django.conf import settings
 class StaticStorage(S3StaticStorage):
     location = settings.AWS_STATIC_LOCATION
 
+    def __init__(self, *args, **kwargs):
+        kwargs['custom_domain'] = settings.AWS_CLOUDFRONT_DOMAIN
+        super(StaticStorage, self).__init__(*args, **kwargs)
 
 class MediaStorage(S3Boto3Storage):
     location = settings.PUBLIC_MEDIA_LOCATION
     file_overwrite = True
+
+    def __init__(self, *args, **kwargs):
+        kwargs['custom_domain'] = settings.AWS_CLOUDFRONT_DOMAIN
+        super(MediaStorage, self).__init__(*args, **kwargs)
 
 
 # StaticRootS3BotoStorage = lambda: S3Boto3Storage(location='static')
