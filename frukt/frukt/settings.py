0cecd04cfda48dfa1c8e8c8d076768d53fad1824
Robb <56496611+robb666@users.noreply.github.com>
2023-02-01 17:13:17 +0100

    up

--- frukt/frukt/settings.py
+++ frukt/frukt/settings.py
@@ -201,7 +201,7 @@ CSRF_TRUSTED_ORIGINS = ['https://blackcurrant-wholesale.com', 'https://blackcurr
 
 
 ## deployment
-CSRF_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = False
 SECURE_SSL_REDIRECT = False
 
