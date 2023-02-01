5f86689b6bf1e1d0f8bf7efb3713de10a5fc3c3f
Robb <56496611+robb666@users.noreply.github.com>
2023-02-01 15:33:21 +0100

    Update settings.py

--- frukt/frukt/settings.py
+++ frukt/frukt/settings.py
@@ -199,12 +199,11 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
 CSRF_TRUSTED_ORIGINS = ['https://blackcurrant-wholesale.com/']
 
-PREPEND_WWW = False
-BASE_URL = 'https://blackcurrant-wholesale.com/'
+
 ## deployment
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
+SECURE_SSL_REDIRECT = False
 
 
 
