6f66d2ad6b007dc73f9600c403a961d1bd72cae4
Robb <56496611+robb666@users.noreply.github.com>
2023-03-21 10:47:25 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-#python manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 #python manage.py collectstatic --noinput
 
 #DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
